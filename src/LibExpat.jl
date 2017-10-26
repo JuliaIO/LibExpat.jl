14678db20e6264aa19c1be1cb91f83a75032b99c
Grégory Salvan <apieum@gmail.com>
2017-10-26 21:37:02 +0200

    reindent with 4 spaces

--- src/LibExpat.jl
+++ src/LibExpat.jl
@@ -83,11 +83,11 @@ end
 
 
 type XPHandle
-  parser::Union{XML_Parser,Void}
-  pdata::ETree
-  in_cdata::Bool
+    parser::Union{XML_Parser,Void}
+    pdata::ETree
+    in_cdata::Bool
 
-  XPHandle(p) = new(p, ETree(""), false)
+    XPHandle(p) = new(p, ETree(""), false)
 end
 
 parsed_data(xph::XPHandle) = root(xph.pdata)
@@ -114,8 +114,8 @@ end
 
 
 function xp_close(xph::XPHandle)
-  if (xph.parser != nothing)    XML_ParserFree(xph.parser) end
-  xph.parser = nothing
+    if (xph.parser != nothing)    XML_ParserFree(xph.parser) end
+    xph.parser = nothing
 end
 
 
@@ -233,52 +233,52 @@ end
 
 
 function xp_parse(xph::XPHandle, txt::AbstractString)
-  try
-    rc = XML_Parse(xph.parser, txt, sizeof(txt), 1)
-    (rc != XML_STATUS_OK) && error("Error parsing document : $rc")
+    try
+        rc = XML_Parse(xph.parser, txt, sizeof(txt), 1)
+        (rc != XML_STATUS_OK) && error("Error parsing document : $rc")
 
-    return parsed_data(xph)
-  catch e
-    stre = string(e)
-    (err, line, column, pos) = xp_geterror(xph)
-    rethrow("$e, $err, $line, $column, $pos")
+        return parsed_data(xph)
+    catch e
+        stre = string(e)
+        (err, line, column, pos) = xp_geterror(xph)
+        rethrow("$e, $err, $line, $column, $pos")
 
-  finally
-    xp_close(xph)
-  end
+    finally
+        xp_close(xph)
+    end
 end
 
 function xp_parse(p::XML_Parser, txt::AbstractString)
-  cb_start_cdata = cfunction(start_cdata, Void, Tuple{Ptr{Void}})
-  cb_end_cdata = cfunction(end_cdata, Void, Tuple{Ptr{Void}})
-  cb_cdata = cfunction(cdata, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
-  cb_comment = cfunction(comment, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
-  cb_default = cfunction(default, Void,  Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
-  cb_default_expand = cfunction(default_expand, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
-  cb_start_element = cfunction(start_element, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Ptr{Ptr{UInt8}}})
-  cb_end_element = cfunction(end_element, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
-  cb_start_namespace = cfunction(start_namespace, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Ptr{UInt8}})
-  cb_end_namespace = cfunction(end_namespace, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
+    cb_start_cdata = cfunction(start_cdata, Void, Tuple{Ptr{Void}})
+    cb_end_cdata = cfunction(end_cdata, Void, Tuple{Ptr{Void}})
+    cb_cdata = cfunction(cdata, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
+    cb_comment = cfunction(comment, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
+    cb_default = cfunction(default, Void,  Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
+    cb_default_expand = cfunction(default_expand, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Cint})
+    cb_start_element = cfunction(start_element, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Ptr{Ptr{UInt8}}})
+    cb_end_element = cfunction(end_element, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
+    cb_start_namespace = cfunction(start_namespace, Void, Tuple{Ptr{Void}, Ptr{UInt8}, Ptr{UInt8}})
+    cb_end_namespace = cfunction(end_namespace, Void, Tuple{Ptr{Void}, Ptr{UInt8}})
 
-  xph = XPHandle(p)
-  p_xph = pointer_from_objref(xph)
-  XML_SetUserData(p, p_xph);
+    xph = XPHandle(p)
+    p_xph = pointer_from_objref(xph)
+    XML_SetUserData(p, p_xph);
 
-  XML_SetCdataSectionHandler(p, cb_start_cdata, cb_end_cdata)
-  XML_SetCharacterDataHandler(p, cb_cdata)
-  XML_SetCommentHandler(p, cb_comment)
-  XML_SetDefaultHandler(p, cb_default)
-  XML_SetDefaultHandlerExpand(p, cb_default_expand)
-  XML_SetElementHandler(p, cb_start_element, cb_end_element)
-#    XML_SetExternalEntityRefHandler(p, f_ExternaEntity)
-  XML_SetNamespaceDeclHandler(p, cb_start_namespace, cb_end_namespace)
-#    XML_SetNotationDeclHandler(p, f_NotationDecl)
-#    XML_SetNotStandaloneHandler(p, f_NotStandalone)
-#    XML_SetProcessingInstructionHandler(p, f_ProcessingInstruction)
-#    XML_SetUnparsedEntityDeclHandler(p, f_UnparsedEntityDecl)
-#    XML_SetStartDoctypeDeclHandler(p, f_StartDoctypeDecl)
+    XML_SetCdataSectionHandler(p, cb_start_cdata, cb_end_cdata)
+    XML_SetCharacterDataHandler(p, cb_cdata)
+    XML_SetCommentHandler(p, cb_comment)
+    XML_SetDefaultHandler(p, cb_default)
+    XML_SetDefaultHandlerExpand(p, cb_default_expand)
+    XML_SetElementHandler(p, cb_start_element, cb_end_element)
+    # XML_SetExternalEntityRefHandler(p, f_ExternaEntity)
+    XML_SetNamespaceDeclHandler(p, cb_start_namespace, cb_end_namespace)
+    # XML_SetNotationDeclHandler(p, f_NotationDecl)
+    # XML_SetNotStandaloneHandler(p, f_NotStandalone)
+    # XML_SetProcessingInstructionHandler(p, f_ProcessingInstruction)
+    # XML_SetUnparsedEntityDeclHandler(p, f_UnparsedEntityDecl)
+    # XML_SetStartDoctypeDeclHandler(p, f_StartDoctypeDecl)
 
-  return xp_parse(xph, txt)
+    return xp_parse(xph, txt)
 end
 
 xp_parse(txt::AbstractString) = xp_parse(XML_ParserCreate(C_NULL), txt)
